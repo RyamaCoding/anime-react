ea314bde9764beda3b678ae971ae92240f087f9a
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-06-30 13:30:53 +0100

    adding search functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,69 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Pages.css'
 import Image from '../assets/megumipng.gif'
 import Logo from '../assets/rc_logo_black.png'
 import Search from '../assets/magnifying-glass-solid.svg'
 import Spinner from '../assets/spinner-solid.svg'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Home = () => {
+  const [cards, setCards] = useState([])
+  const [search, setSearch] = useState('')
 
-  let navigate = useNavigate()
 
-  return (
-<div>
-    <nav class="nav__bar">
-      <div class="content-wrapper">
-        <a href="https://github.com/RyamaCoding" target="_blank">
-          <img src={Logo} class="personal__logo" alt="GitHub" />
-        </a>
-        <ul class="nav__list">
-          <li class="nav__link">
-            <a href="#" class="nav__text nav__text-effect"> <span class="blue">Home</span></a>
-          </li>
-          <li class="nav__link">
-            <a onClick={() => navigate('/posts')} class="nav__text nav__text-effect2">Find your anime</a>
-          </li>
-          <li class="nav__link">
-            <button href="" id="contactButton" onclick="showErrorMessage()" class="btn__contact">CONTACT</button>
-          </li>
-        </ul>
+    const fetchAnime = async (query) => {
+      try {
+        const { data } = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}`);
+        setCards(data);
+        navigate('/posts', { state: { cards: data } });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    const handleSearch = (e) => {
+      e.preventDefault();
+      fetchAnime(search);
+    }
+    
+    
+    let navigate = useNavigate()
+    
+    return (
+      <div>
+      <nav className="nav__bar">
+        <div className="content-wrapper">
+          <a href="https://github.com/RyamaCoding">
+            <img src={Logo} className="personal__logo" alt="GitHub" />
+          </a>
+          <ul className="nav__list">
+            <li className="nav__link">
+              <a href="#" className="nav__text nav__text-effect"> <span className="blue">Home</span></a>
+            </li>
+            <li className="nav__link">
+              <a href="/posts" className="nav__text nav__text-effect2">Find your anime</a>
+            </li>
+            <li className="nav__link">
+              <button id="contactButton" className="btn__contact">CONTACT</button>
+            </li>
+          </ul>
 
-      <button class="btn__menu" onclick="openMenu()">
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="menu__backdrop">
-        <button class="btn__menu btn__menu--close" onclick="closeMenu()">
-          <i class="fas fa-times"></i>
-        </button>
-        <ul class="menu__links">
-          <li class="menu__list"><a href="#" class="menu__link nav__text-effect3" onclick="closeMenu()">Home</a></li>
-          <li class="menu__list"><a onClick={() => navigate('/posts')} class="menu__link nav__text-effect3" onclick="closeMenu()">Find your anime</a></li>
-          <li class="menu__list"><a href="#" class="menu__link no-cursor nav__text-effect3" onclick="closeMenu()">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-    <section id="landing-page">
-      <div class="container">
-        <div class="landing__box">
-          <div class="landing__description">
-            <h1 class="landing__title">Welcome to the UK's #1 anime search engine</h1>
-            <h2 class="landing__sub-title">Find the <span class="blue2">anime</span> of your <span class="blue2">choice</span></h2>
-            <form action="anime.html" method="GET" class="landing__form">
-              <input type="text" name="search" id="search-input" placeholder="Search for your favourite animes here" class="landing__search"></input>
-              <button type="submit" id="search-button" class="landing__btn btn__loading" onclick="startLoading()">
-                <img class="btn__img" src={Search}></img>
-                <img class="btn__img--loading" src={Spinner} alt=""></img>
-              </button>
-            </form>
+          <button className="btn__menu" >
+            <i className="fas fa-bars"></i>
+          </button>
+          <div className="menu__backdrop">
+            <button className="btn__menu btn__menu--close" >
+              <i className="fas fa-times"></i>
+            </button>
+            <ul className="menu__links">
+              <li className="menu__list"><a href="#" className="menu__link nav__text-effect3">Home</a></li>
+              <li className="menu__list"><a href="/posts" className="menu__link nav__text-effect3">Find your anime</a></li>
+              <li className="menu__list"><a href="#" className="menu__link no-cursor nav__text-effect3">Contact</a></li>
+            </ul>
           </div>
-          <img src={Image} alt="" class="landing__img" />
         </div>
-      </div>
-    </section>
-  </div>
+      </nav>
+
+      <section id="landing-page">
+        <div className="container">
+          <div className="landing__box">
+            <div className="landing__description">
+              <h1 className="landing__title">Welcome to the UK's #1 anime search engine</h1>
+              <h2 className="landing__sub-title">Find the <span className="blue2">anime</span> of your <span className="blue2">choice</span></h2>
+              <form onSubmit={handleSearch} className="landing__form">
+                <input type="text" name="search" id="search-input" placeholder="Search for your favourite animes here" className="landing__search" value={search} onChange={(e) => setSearch(e.target.value)}></input>
+                <button type="submit" id="search-button" className="landing__btn btn__loading">
+                  <img className="btn__img" src={Search} alt=""></img>
+                  <img className="btn__img--loading" src={Spinner} alt=""></img>
+                </button>
+              </form>
+            </div>
+            <img src={Image} alt="" className="landing__img" />
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -17,46 +17,46 @@ const styles = {
 
   return (
       <div id="anime__body">
-    <nav class="nav__bar nav__bar--anime" style={styles}>
-        <div class="nav__container">
-            <div class="anime__content-wrapper">
+    <nav className="nav__bar nav__bar--anime" style={styles}>
+        <div className="nav__container">
+            <div className="anime__content-wrapper">
                 <a href="https://github.com/RyamaCoding" target="_blank">
-                    <img src={Logo} class="anime__personal--logo" alt="GitHub" />
+                    <img src={Logo} className="anime__personal--logo" alt="GitHub" />
                 </a>
-                <ul class="nav__list">
-                    <li class="nav__link">
-                        <a onClick={() => navigate('/')} class="nav__text nav__text-effect--white"> <span class="white">Home</span></a>
+                <ul className="nav__list">
+                    <li className="nav__link">
+                        <a onClick={() => navigate('/')} className="nav__text nav__text-effect--white"> <span className="white">Home</span></a>
                     </li>
-                    <li class="nav__link">
-                        <a href="#" class="nav__text nav__text-effect--white"> <span class="white">Find your anime</span></a>
+                    <li className="nav__link">
+                        <a href="#" className="nav__text nav__text-effect--white"> <span className="white">Find your anime</span></a>
                     </li>
-                    <li class="nav__link">
-                        <button href="" id="contactButton" onclick="showErrorMessage()" class="btn__contact--anime">CONTACT</button>
+                    <li className="nav__link">
+                        <button href="" id="contactButton" className="btn__contact--anime">CONTACT</button>
                     </li>
                 </ul>
                 
-                <button class="btn__menu" onclick="openMenu()">
-                    <i class="fas fa-bars white-bars"></i>
+                <button className="btn__menu" >
+                    <i className="fas fa-bars white-bars"></i>
                 </button>
-                <div class="menu__backdrop--anime">
-                    <button class="btn__menu btn__menu--close" onclick="closeMenu()">
-                        <i class="fas fa-times"></i>
+                <div className="menu__backdrop--anime">
+                    <button className="btn__menu btn__menu--close" >
+                        <i className="fas fa-times"></i>
                     </button>
-                    <ul class="menu__links">
-                        <li class="menu__list"><a href="index.html" class="menu__link nav__text-effect3" onclick="closeMenu()">Home</a></li>
-                        <li class="menu__list"><a href="#anime" class="menu__link nav__text-effect3" onclick="closeMenu()">Find your anime</a></li>
-                        <li class="menu__list"><a href="#" class="menu__link no-cursor nav__text-effect3" onclick="closeMenu()">Contact</a></li>
+                    <ul className="menu__links">
+                        <li className="menu__list"><a href="index.html" className="menu__link nav__text-effect3">Home</a></li>
+                        <li className="menu__list"><a href="#anime" className="menu__link nav__text-effect3">Find your anime</a></li>
+                        <li className="menu__list"><a href="#" className="menu__link no-cursor nav__text-effect3">Contact</a></li>
                     </ul>
                 </div>
             </div>
             
-            <div class="nav__description">
-                <h2 class="landing__sub-title nav__sub-title">Find the <span class="white2">anime</span> of your <span class="white2">choice</span></h2>
-                <form action="" class="landing__form" onsubmit="handleSearch(event)">
-                    <input type="text" id="search-input" placeholder="Search for your favourite animes here" class="landing__search" />
-                    <button type="submit" id="search-button" class="landing__btn" onclick="startLoading()">
-                        <img class="btn__img" src={Search}></img>
-                        <img class="btn__img--loading" src={Spinner} alt=""></img>
+            <div className="nav__description">
+                <h2 className="landing__sub-title nav__sub-title">Find the <span className="white2">anime</span> of your <span className="white2">choice</span></h2>
+                <form action="" className="landing__form">
+                    <input type="text" id="search-input" placeholder="Search for your favourite animes here" className="landing__search" />
+                    <button type="submit" id="search-button" className="landing__btn">
+                        <img className="btn__img" src={Search}></img>
+                        <img className="btn__img--loading" src={Spinner} alt=""></img>
                     </button>
                 </form>
             </div>
@@ -64,8 +64,8 @@ const styles = {
         </nav>
 
         <section id="anime">
-            <div class="anime__row">
-                <div class="card__wrapper">
+            <div className="anime__row">
+                <div className="card__wrapper">
                 </div>
             </div>
         </section>
